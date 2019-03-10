6090638758fb44b3bbfb58000b5187d6433300bb
metigel94 <leon-mueller@hotmail.de>
2019-03-10 17:15:30 +0100

    Managed to send the positions to the .py-server
    Next step will be to split up the string and take that as input arguments for the a-star function and return the path to c#.

---
+++ a-star/Assets/ServerCode/HelloClient.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using AsyncIO;
+using NetMQ;
+using NetMQ.Sockets;
+
+public class HelloClient : MonoBehaviour
+{
+    private HelloRequester _helloRequester;
+    public bool SendPack = true;
+    public Quaternion joint;
+    public Transform pos;
+    string message;
+ 
+    
+    void Update()
+    {
+        if (SendPack)
+        {
+            string positions = GameObject.Find("SeekerCharacter").transform.position.ToString() + "   " + GameObject.Find("TargetCharacter").transform.position.ToString();
+
+            _helloRequester.messageToSend = positions;
+           
+            
+
+
+            _helloRequester.Continue();
+
+        } else if (!SendPack)
+        {
+            _helloRequester.Pause();
+        }
+    }
+
+    private void Start()
+    {
+        _helloRequester = new HelloRequester();
+        _helloRequester.Start();
+    }
+
+    private void OnDestroy()
+    {
+        _helloRequester.Stop();
+    }
+}
